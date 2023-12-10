96464d0392c86d7d8b0742fdddf46112d5329bb9
lauracmd <150671960+lauracmd@users.noreply.github.com>
2023-12-10 15:12:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // Search menu
 
 const openSearchButton = document.querySelector("header button:nth-of-type(1)");
-const searchMenu = document.querySelector("header nav");
-const sluitSearchButton = document.querySelector("header nav button");
+const searchMenu = document.querySelector("header form");
+const sluitSearchButton = document.querySelector("header form button");
 
 openSearchButton.onclick = toonSearchMenu;
 
@@ -10,10 +10,12 @@ sluitSearchButton.onclick = sluitSearchMenu;
 
 function toonSearchMenu() {
     searchMenu.classList.add("inBeeld");
+    document.documentElement.classList.add("inBeeld");
 }
 
 function sluitSearchMenu() {
     searchMenu.classList.remove("inBeeld");
+    document.documentElement.classList.remove("inBeeld");
 }
 
 
@@ -36,4 +38,16 @@ function toonNavMenu() {
 function sluitNavMenu() {
     navMenu.classList.remove("inBeeld");
     document.documentElement.classList.remove("inBeeld");
+}
+
+
+// Open Programs lijst met science en education
+
+var programs = document.querySelector("header nav > ul > li:nth-of-type(2)");
+var programsLi = document.querySelector("header nav ul li:nth-of-type(2) > ul");
+
+programs.onclick = togglePrograms;
+
+function togglePrograms() {
+	programsLi.classList.toggle("open");
 }
